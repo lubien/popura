770fc17d00b88e8d49f02fdd635c479ca8f1062f
lubien <lubien1996@gmail.com>
2016-06-17 23:19:48 -0300

    Add jsdocs to utils

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,22 @@
+/**
+ * @module utils/request
+ */
+
 import got from 'got';
 import xml2js from 'xml2js-es6-promise';
 import cleanApiData from './clean-api-data';
 
 const debug = require('debug')('node-anime-list:request');
 
+/**
+ * @function requestRaw
+ * @description Request a raw response.body from a MAL webpage
+ *
+ * @param  {authToken} authToken
+ * @param  {string} url = '/' - Pathname
+ * @param  {object} query = {} - Query object
+ * @return {Promise} - A Promise that resolves to the raw request body
+ */
 export function requestRaw(authToken, url = '/', query = {}) {
 	debug('Requesting %s with query', url, query);
 	debug('Using auth:', `Basic ${authToken}`);
@@ -17,6 +30,18 @@ export function requestRaw(authToken, url = '/', query = {}) {
 
 // TODO: function requestHtml()
 
+/**
+ * @function requestApi
+ * @description Request MAL's API XML, then parses as JSON and clean it
+ * @see module:utils/cleanApiData
+ * @see module:utils/flattenObject
+ *
+ * @param  {authToken} authToken
+ * @param  {string} url = '/' - Pathname
+ * @param  {object} query = {} - Query object
+ * @return {Promise} - A Promise that resolves to a parsed as JSON and
+ * cleaned version of MAL's API response
+ */
 export function requestApi(authToken, url = '/', query = {}) {
 	if (!authToken) {
 		debug('Not authenticated');
