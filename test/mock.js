262aa0daaca96058358f174151dda6553dc04913
lubien <lubien1996@gmail.com>
2016-06-19 03:32:05 -0300

    test/mock now exports state and testing constants

--- test/mock.js
+++ test/mock.js
@@ -17,3 +17,14 @@ export const authToken = generateAuthToken(
   process.env.MAL_USER,
   process.env.MAL_PASS
 );
+
+export const state = {
+	username: process.env.MAL_USER,
+	password: process.env.MAL_PASS,
+	authToken,
+};
+
+// Tachumaru Gekijou
+export const TEST_ANIME_ID = process.env.TEST_ANIME_ID || 9562;
+// Junshin Miracle 100%
+export const TEST_MANGA_ID = process.env.TEST_MANGA_ID || 94483;
