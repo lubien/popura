b3b3f8963c26c97fe620ebef670ba7cd765729d0
lubien <lubien1996@gmail.com>
2016-06-19 23:41:29 -0300

    Do not use AVA's .notThrows
    .notThrows does not give us a verbose exception. Just await the result and it will throw if fail.

--- test/composers/listable.test.js
+++ test/composers/listable.test.js
@@ -4,27 +4,27 @@ import {state, TEST_ANIME_ID, TEST_MANGA_ID} from '../mock';
 
 const instance = listable(state);
 
-test(`Can get user's anime and manga lists`, t => {
-	t.notThrows(instance.getAnimeList());
-	t.notThrows(instance.getMangaList());
+test(`Can get user's anime and manga lists`, async () => {
+	await instance.getAnimeList();
+	await instance.getMangaList();
 });
 
-test(`Can get other user's anime and manga lists`, t => {
-	t.notThrows(instance.getAnimeList('xinil'));
-	t.notThrows(instance.getMangaList('xinil'));
+test(`Can get other user's anime and manga lists`, async () => {
+	await instance.getAnimeList('xinil');
+	await instance.getMangaList('xinil');
 });
 
-test.serial('Can add animes and mangas to lists', t => {
-	t.notThrows(instance.addAnime(TEST_ANIME_ID));
-	t.notThrows(instance.addManga(TEST_MANGA_ID));
+test.serial('Can add animes and mangas to lists', async () => {
+	await instance.addAnime(TEST_ANIME_ID);
+	await instance.addManga(TEST_MANGA_ID);
 });
 
-test.serial('Can modify animes and mangas in lists', t => {
-	t.notThrows(instance.updateAnime(TEST_ANIME_ID, {score: 10}));
-	t.notThrows(instance.updateManga(TEST_MANGA_ID, {score: 10}));
+test.serial('Can modify animes and mangas in lists', async () => {
+	await instance.updateAnime(TEST_ANIME_ID, {score: 10});
+	await instance.updateManga(TEST_MANGA_ID, {score: 10});
 });
 
-test.serial('Can delete animes and mangas from lists', t => {
-	t.notThrows(instance.deleteAnime(TEST_ANIME_ID));
-	t.notThrows(instance.deleteManga(TEST_MANGA_ID));
+test.serial('Can delete animes and mangas from lists', async () => {
+	await instance.deleteAnime(TEST_ANIME_ID);
+	await instance.deleteManga(TEST_MANGA_ID);
 });
--- test/composers/searchable.test.js
+++ test/composers/searchable.test.js
@@ -1,10 +1,10 @@
 import test from 'ava';
 import {instance} from '../mock';
 
-test('Can search for animes', t => {
-	t.notThrows(instance.searchAnimes('Full Metal'));
+test('Can search for animes', async () => {
+	await instance.searchAnimes('Full Metal');
 });
 
-test('Can search for mangas', t => {
-	t.notThrows(instance.searchMangas('Full Metal'));
+test('Can search for mangas', async () => {
+	await instance.searchMangas('Full Metal');
 });
