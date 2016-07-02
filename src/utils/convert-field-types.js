75411af1ded822d517b0581cbd6b0e1239bc9b74
lubien <lubien1996@gmail.com>
2016-07-02 16:25:52 -0300

    Add missing numeric fields to conversion

--- src/utils/convert-field-types.js
+++ src/utils/convert-field-types.js
@@ -2,7 +2,10 @@ const numericFields = [
 	'id', 'episodes', 'chapters', 'score', 'user_id', 'user_watching', 'user_completed',
 	'user_onhold', 'user_dropped', 'user_plantowatch', 'user_days_spent_watching',
 	'series_animedb_id', 'series_type', 'series_episodes', 'series_status', 'my_id',
-	'my_status', 'my_rewatching', 'my_rewatching_ep', 'my_last_update',
+	'my_status', 'my_rewatching', 'my_rewatching_ep', 'my_last_update', 'series_mangadb_id',
+	'series_chapters', 'series_volumes', 'my_read_chapters', 'my_read_volumes', 'my_score',
+	'my_rereadingg', // Notice that the double 'g' at the end is not a typo, it's MAL return!
+	'my_rereading_chap', 'user_reading', 'user_plantoread',
 ];
 const dateFields = [
 	'start_date', 'end_date', 'series_start', 'series_end', 'my_last_updated', 'my_start_date', 'my_finish_date',
