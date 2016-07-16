9ac583613a4989f0b161fdf6c06ce0c0ff59e012
lubien <lubien1996@gmail.com>
2016-07-15 21:45:22 -0300

    Use switch-case in convert-field-types

--- src/utils/convert-field-types.js
+++ src/utils/convert-field-types.js
@@ -1,8 +1,5 @@
 import {splitBy, showTwoDigit} from './';
 
-const commaSeparatedArrayFields = ['tags', 'my_tags'];
-const semicolonSeparatedArrayFields = ['synonyms', 'series_synonyms'];
-
 /**
  * Convert MAL's API fields to proper types
  *
@@ -15,19 +12,32 @@ export default function convertFieldTypes(obj) {
 	for (const key of Object.keys(obj)) {
 		const value = obj[key];
 
-		if (key === 'my_last_updated') {
-			// Sometimes it use timesptamp! Fuck U MAL API
-			const date = new Date(Number(value * 1000));
-			result[key] =
-				`${date.getUTCFullYear()}-${showTwoDigit(date.getUTCMonth())}-${showTwoDigit(date.getUTCDate())}`;
-		} else if (commaSeparatedArrayFields.indexOf(key) !== -1) {
-			result[key] = splitBy(',', value);
-		} else if (semicolonSeparatedArrayFields.indexOf(key) !== -1) {
-			result[key] = splitBy(';', value);
-		} else {
-			result[key] = value;
+		switch (key) {
+			case 'my_last_updated': {
+				// Sometimes it use timesptamp! Fuck U MAL API
+				const date = new Date(Number(value * 1000));
+				result[key] =
+					`${date.getUTCFullYear()}-${showTwoDigit(date.getUTCMonth())}-${showTwoDigit(date.getUTCDate())}`;
+				break;
+			}
+
+			case 'tags':
+			case 'my_tags': {
+				result[key] = splitBy(',', value);
+				break;
+			}
+
+			case 'synonyms':
+			case 'series_synonyms': {
+				result[key] = splitBy(';', value);
+				break;
+			}
+
+			default: {
+				result[key] = value;
+			}
 		}
-	}
+	};
 
 	return result;
 }
